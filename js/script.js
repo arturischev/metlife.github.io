fddaabf30c4e2b94504db65d0cc92cba6f46b286
arturischev <artur@375.by>
2020-10-14 15:38:08 +0300

    slider repair

--- js/script.js
+++ js/script.js
@@ -18,48 +18,15 @@ $(document).ready(function () {
         $('body').toggleClass('lock');
     });
 
-    /* $('.get-more').on('click', function () {
-        $('.modal').css({
-            'opacity': 1,
-            'visibility': 'visible'
-        });
-        $('.myModal__content').css({
-            'opacity': 1,
-            'transform': 'perspective(600px) translate(0px, 0px) rotateX(0deg)'
-        });
-    });
-
-    $('.myModal__close, .myModal__area').on('click', function () {
-        $('.modal').css({
-            'opacity': 0,
-            'visibility': 'hidden'
-        });
-        $('.myModal__content').css({
-            'opacity': 0,
-            'transform': 'perspective(600px) translate(0px, -100%) rotateX(45deg)'
-        });
-    }); */
-
     $('.slider').slick({
-        arrows: true,
+        // arrows: true,
         dots: true,
-        adaptiveHeight: false,
         slidesToShow: 3,
         slidesToScroll: 1,
-        speed: 1000,
-        easing: 'linear',
-        infinite: true,
-        initialSlide: 0,
+        speed: 800,
         autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDotsHover: true,
-        draggable: false,
-        swipe: true,
-        touchThreshold: 5,
-        touchMove: true,
-        waitForAnimate: true,
+        autoplaySpeed: 2000,
+        waitForAnimate: false,
         responsive: [{
                 breakpoint: 992,
                 settings: {
